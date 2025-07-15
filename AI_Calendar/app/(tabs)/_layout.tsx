c5987eefca55079930b4ad0f63030c0ffc1ae465
Chase Naples <cnaples79@gmail.com>
2025-07-15 00:53:41 -0400

    update

---
+++ AI_Calendar/app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="comment" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
